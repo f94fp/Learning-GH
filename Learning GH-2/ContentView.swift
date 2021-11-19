d6b6e420f798c4fabb7ea0dc22299a695ce7b3ae
Fredrik Pahlm <fredrik.pahlm@sensolution.se>
2021-11-18 16:45:03 -0800

    Initial Commit

---
+++ Learning GH-2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Learning GH-2
+//
+//  Created by Fredrik Pahlm on 2021-11-18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Learning GH-2/Learning_GH_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_GH_2App.swift
+//  Learning GH-2
+//
+//  Created by Fredrik Pahlm on 2021-11-18.
+//
+
+import SwiftUI
+
+@main
+struct Learning_GH_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
